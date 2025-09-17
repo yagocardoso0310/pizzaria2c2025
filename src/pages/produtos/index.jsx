f4627d338c9860825b57c8ddc39797b1e0ea9abd
yagocardoso0310 <rm94755@estudante.fieb.edu.br>
2025-09-17 09:12:43 -0300

    buscar produtos

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,11 +1,16 @@
-const Produtos = () => {
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-    // Objeto da lista de pizzas
-    const pizzas = [
-        'Pizza Muçarela',
-        'Pizza Calabresa',
-        'Pizza Portuguesa'
-    ]
+const Produtos = () => {
+    const [pizzas, setPizzas] = useState([])
+    // Consumir rota com lista de produtos
+    axios.get("http://viacep.com.br/wss010110000/json")
+    .then(response=>{
+        console.log (response.data)
+    })
+    .catch(error=>{console.log(error)})
+
+
     // Iteração da lista de pizzas - pizza a pizza (um a um)
     const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
  
@@ -13,14 +18,7 @@ const Produtos = () => {
         <div>
             <h3>Listagem de Produtos</h3>
             <ul>
-                <li>Pizza de Calabresa</li>
-                <li>Pizza de Muçarela</li>
-                <li>Pizza de Queijo</li>
-                <li>Pizza de Queijo</li>
-                <li>Pizza de Queijo</li>
-                <li>Pizza de Queijo</li>
-                <li>Pizza de Queijo</li>
- 
+                {listaPizzas}
             </ul>
         </div>
  
